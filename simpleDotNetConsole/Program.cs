bb6d8ee5f8a871f7d1931edfea76c3efea477268
Jacek Zietek <jkzk@gft.com>
2020-05-28 10:48:41 +0200

    Initial commit

---
+++ simpleDotNetConsole/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace simpleDotNetConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello World 2!");
+        }
+    }
+}
