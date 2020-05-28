9c43c66c0cf91f65d1205eb8e07d832f39b1a3f9
Jacek Zietek <jkzk@gft.com>
2020-05-28 12:41:45 +0200

    Simple random logger with Docker scaffolding

--- simpleDotNetConsole/Program.cs
+++ simpleDotNetConsole/Program.cs
@@ -1,14 +1,64 @@
 ﻿using System;
+using Serilog;
 
 namespace simpleDotNetConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //Args format: ITERATIONS_COUNT ITERATION_DELAY
         {
-            Console.WriteLine("Hello World!");
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File("simpleDotNetConsole.log")
+                .WriteTo.Console()
+                .CreateLogger();
 
-            Console.WriteLine("Hello World 2!");
+            if (args.Length < 2)
+            {
+                Log.Error("Missing expected command line arguments: ITERATIONS_COUNT ITERATION_DELAY_MILLISECONDS");
+                return;
+            }
+            int iterationsCount = 0;
+            if(!int.TryParse(args[0], out iterationsCount))
+            {
+                Log.Error("Invalid iteartaions count paramaeter. Termianting!");
+                return;
+            }
+
+            int iterationDelay = 0;
+            if(!int.TryParse(args[1], out iterationDelay))
+            {
+                Log.Error("Invalid iteartaions delay paramaeter. Termianting!");
+                return;
+            }
+
+            Random r = new Random(DateTime.Now.Millisecond);
+
+            int counter = 0;
+            while (counter < iterationsCount)
+            {
+                ShowRandomLogMessage(r);
+
+                System.Threading.Thread.Sleep(iterationDelay);
+                counter++;
+            }
+        }
+
+        private static void ShowRandomLogMessage(Random r)
+        {
+            int n = r.Next(100);
+
+            if (n < 10)
+            {
+                Log.Error($"This is some random error number {n}");
+            }
+            if (n < 50)
+            {
+                Log.Warning($"This is some random warning number {n}");
+            }
+            else
+            {
+                Log.Information($"This is some random info number {n}");
+            }
         }
     }
 }
